4c7ac00ed30f86f033ce6d8af722ff982aa815c1
Ooms Aurélien <aurelien.ooms@gmail.com>
2013-09-29 19:26:47 +0200

    enum test

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,21 @@
 
 class HelloWorld{
+
+	public enum Token {
+		SUNDAY, MONDAY, TUESDAY, WEDNESDAY,
+		THURSDAY, FRIDAY, SATURDAY 
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
+
+		String[] myStringArray = {"a","b","c"};
+
+		System.out.println(myStringArray);
+
+		System.out.println(HelloWorld.Token.SUNDAY);
+
+
+
 	}
 }
