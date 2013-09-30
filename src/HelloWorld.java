28d21d0e33066996393f8f5ed3932ed105e4944f
Ooms Aurélien <aurelien.ooms@gmail.com>
2013-09-30 23:09:02 +0200

    dividing into packages

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,3 @@
-import java.util.Map;
-import java.util.HashMap;
-
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -21,90 +18,16 @@ public class HelloWorld{
 	
 	public static void main(String[] args) throws FileNotFoundException, IOException{
 
-		Map<SCobol.LexicalUnit,String> map = new HashMap<SCobol.LexicalUnit,String>() {
-			{
-				put(SCobol.LexicalUnit.IDENTIFICATION_KEYWORD, "identification");
-				put(SCobol.LexicalUnit.DIVISION_KEYWORD, "division");
-				put(SCobol.LexicalUnit.PROGRAM_ID_KEYWORD, "program\\-id");
-				put(SCobol.LexicalUnit.AUTHOR_KEYWORD, "author");
-				put(SCobol.LexicalUnit.DATE_WRITTEN_KEYWORD, "date\\-written");
-				put(SCobol.LexicalUnit.ENVIRONMENT_KEYWORD, "environment");
-				put(SCobol.LexicalUnit.CONFIGURATION_KEYWORD, "configuration");
-				put(SCobol.LexicalUnit.SECTION_KEYWORD, "section");
-				put(SCobol.LexicalUnit.SOURCE_COMPUTER_KEYWORD, "source\\-computer");
-				put(SCobol.LexicalUnit.OBJECT_COMPUTER_KEYWORD, "object\\-computer");
-				put(SCobol.LexicalUnit.DATA_KEYWORD, "data");
-				put(SCobol.LexicalUnit.WORKING_STORAGE_KEYWORD, "working\\-storage");
-				put(SCobol.LexicalUnit.PIC_KEYWORD, "pic");
-				put(SCobol.LexicalUnit.VALUE_KEYWORD, "value");
-				put(SCobol.LexicalUnit.PROCEDURE_KEYWORD, "procedure");
-				put(SCobol.LexicalUnit.END_KEYWORD, "end");
-				put(SCobol.LexicalUnit.PROGRAM_KEYWORD, "program");
-				put(SCobol.LexicalUnit.STOP_KEYWORD, "stop");
-				put(SCobol.LexicalUnit.RUN_KEYWORD, "run");
-				put(SCobol.LexicalUnit.MOVE_KEYWORD, "move");
-				put(SCobol.LexicalUnit.TO_KEYWORD, "to");
-				put(SCobol.LexicalUnit.GIVING_KEYWORD, "giving");
-
-				put(SCobol.LexicalUnit.COMPUTE_KEYWORD, "compute");
-				put(SCobol.LexicalUnit.ADD_KEYWORD, "add");
-				put(SCobol.LexicalUnit.SUBSTRACT_KEYWORD, "substract");
-				put(SCobol.LexicalUnit.MULTIPLY_KEYWORD, "multiply");
-				put(SCobol.LexicalUnit.DIVIDE_KEYWORD, "divide");
-				put(SCobol.LexicalUnit.FROM_KEYWORD, "from");
-
-				put(SCobol.LexicalUnit.NOT_KEYWORD, "not");
-				put(SCobol.LexicalUnit.TRUE_KEYWORD, "true");
-				put(SCobol.LexicalUnit.FALSE_KEYWORD, "false");
-				put(SCobol.LexicalUnit.AND_KEYWORD, "and");
-				put(SCobol.LexicalUnit.OR_KEYWORD, "or");
-
-				put(SCobol.LexicalUnit.IF_KEYWORD, "if");
-				put(SCobol.LexicalUnit.ELSE_KEYWORD, "else");
-				put(SCobol.LexicalUnit.END_IF_KEYWORD, "end\\-if");
-				put(SCobol.LexicalUnit.PERFORM_KEYWORD, "perform");
-				put(SCobol.LexicalUnit.UNTIL_KEYWORD, "until");
-				put(SCobol.LexicalUnit.ACCEPT_KEYWORD, "accept");
-				put(SCobol.LexicalUnit.DISPLAY_KEYWORD, "display");
-
-				put(SCobol.LexicalUnit.ASTERISK, "\\*");
-				put(SCobol.LexicalUnit.SLASH, "/");
-				put(SCobol.LexicalUnit.DOT, "\\.");
-				put(SCobol.LexicalUnit.COMMA, ",");
-				put(SCobol.LexicalUnit.LEFT_PARENTHESIS, "\\(");
-				put(SCobol.LexicalUnit.RIGHT_PARENTHESIS, "\\)");
-				put(SCobol.LexicalUnit.NEW_LINE, "\\n");
-
-				put(SCobol.LexicalUnit.EQUALS_SIGN, "=");
-				put(SCobol.LexicalUnit.LOWER_SIGN, "<");
-				put(SCobol.LexicalUnit.LOWER_OR_EQUALS, "<=");
-				put(SCobol.LexicalUnit.GREATER_SIGN, ">");
-				put(SCobol.LexicalUnit.GREATER_OR_EQUALS, ">=");
-				put(SCobol.LexicalUnit.MINUS_SIGN, "\\-");
-				put(SCobol.LexicalUnit.PLUS_SIGN, "\\+");
-
-				put(SCobol.LexicalUnit.IDENTIFIER, "[A-Za-z][0-9A-Za-z_\\-]{0,15}");
-				put(SCobol.LexicalUnit.IMAGE, "s?9(?:\\([1-9][0-9]*\\))?(?:v9(?:\\([1-9][0-9]*\\))?)?");
-				put(SCobol.LexicalUnit.INTEGER, "(?:\\+|-)?(?:[1-9][0-9]*)|(?:0)");
-				put(SCobol.LexicalUnit.REAL, "(?:\\+|\\-)?(?:[1-9][0-9]*)|(?:0)(?:\\.[0-9]*[1-9])?");
-				put(SCobol.LexicalUnit.STRING, "’[0-9A-Za-z\\+\\-\\*/:!\\? ]*’");
-
-				put(SCobol.LexicalUnit.COMMENT, "(?:\\*|/).*\\.\\n");
-			}
-		};
-
-
-
 		List<SCobol.LexicalUnit> units = Arrays.asList(SCobol.LexicalUnit.values());
 
-		System.out.println(map);
+		System.out.println(SCobol.PATTERNS);
 
 		String regex = "";
 		Iterator<SCobol.LexicalUnit> it = units.iterator();
 		if(it.hasNext()){
-			regex += "(" + map.get(it.next()) + ")";
+			regex += "(" + SCobol.PATTERNS.get(it.next()) + ")";
 			while(it.hasNext()){
-				regex += "|" + "(" + map.get(it.next()) + ")";
+				regex += "|" + "(" + SCobol.PATTERNS.get(it.next()) + ")";
 			}
 		}
 
--- src/cs/lang/SCobol.java
+++ src/cs/lang/SCobol.java
@@ -1,5 +1,8 @@
 package cs.lang;
 
+import java.util.Map;
+import java.util.HashMap;
+
 public class SCobol{
 	public enum LexicalUnit {
 		COMMENT,
@@ -70,4 +73,77 @@ public class SCobol{
 		STRING,
 		IDENTIFIER
 	}
+
+
+	public static final Map<LexicalUnit,String> PATTERNS = new HashMap<LexicalUnit,String>() {
+		{
+			put(LexicalUnit.IDENTIFICATION_KEYWORD, "identification");
+			put(LexicalUnit.DIVISION_KEYWORD, "division");
+			put(LexicalUnit.PROGRAM_ID_KEYWORD, "program\\-id");
+			put(LexicalUnit.AUTHOR_KEYWORD, "author");
+			put(LexicalUnit.DATE_WRITTEN_KEYWORD, "date\\-written");
+			put(LexicalUnit.ENVIRONMENT_KEYWORD, "environment");
+			put(LexicalUnit.CONFIGURATION_KEYWORD, "configuration");
+			put(LexicalUnit.SECTION_KEYWORD, "section");
+			put(LexicalUnit.SOURCE_COMPUTER_KEYWORD, "source\\-computer");
+			put(LexicalUnit.OBJECT_COMPUTER_KEYWORD, "object\\-computer");
+			put(LexicalUnit.DATA_KEYWORD, "data");
+			put(LexicalUnit.WORKING_STORAGE_KEYWORD, "working\\-storage");
+			put(LexicalUnit.PIC_KEYWORD, "pic");
+			put(LexicalUnit.VALUE_KEYWORD, "value");
+			put(LexicalUnit.PROCEDURE_KEYWORD, "procedure");
+			put(LexicalUnit.END_KEYWORD, "end");
+			put(LexicalUnit.PROGRAM_KEYWORD, "program");
+			put(LexicalUnit.STOP_KEYWORD, "stop");
+			put(LexicalUnit.RUN_KEYWORD, "run");
+			put(LexicalUnit.MOVE_KEYWORD, "move");
+			put(LexicalUnit.TO_KEYWORD, "to");
+			put(LexicalUnit.GIVING_KEYWORD, "giving");
+
+			put(LexicalUnit.COMPUTE_KEYWORD, "compute");
+			put(LexicalUnit.ADD_KEYWORD, "add");
+			put(LexicalUnit.SUBSTRACT_KEYWORD, "substract");
+			put(LexicalUnit.MULTIPLY_KEYWORD, "multiply");
+			put(LexicalUnit.DIVIDE_KEYWORD, "divide");
+			put(LexicalUnit.FROM_KEYWORD, "from");
+
+			put(LexicalUnit.NOT_KEYWORD, "not");
+			put(LexicalUnit.TRUE_KEYWORD, "true");
+			put(LexicalUnit.FALSE_KEYWORD, "false");
+			put(LexicalUnit.AND_KEYWORD, "and");
+			put(LexicalUnit.OR_KEYWORD, "or");
+
+			put(LexicalUnit.IF_KEYWORD, "if");
+			put(LexicalUnit.ELSE_KEYWORD, "else");
+			put(LexicalUnit.END_IF_KEYWORD, "end\\-if");
+			put(LexicalUnit.PERFORM_KEYWORD, "perform");
+			put(LexicalUnit.UNTIL_KEYWORD, "until");
+			put(LexicalUnit.ACCEPT_KEYWORD, "accept");
+			put(LexicalUnit.DISPLAY_KEYWORD, "display");
+
+			put(LexicalUnit.ASTERISK, "\\*");
+			put(LexicalUnit.SLASH, "/");
+			put(LexicalUnit.DOT, "\\.");
+			put(LexicalUnit.COMMA, ",");
+			put(LexicalUnit.LEFT_PARENTHESIS, "\\(");
+			put(LexicalUnit.RIGHT_PARENTHESIS, "\\)");
+			put(LexicalUnit.NEW_LINE, "\\n");
+
+			put(LexicalUnit.EQUALS_SIGN, "=");
+			put(LexicalUnit.LOWER_SIGN, "<");
+			put(LexicalUnit.LOWER_OR_EQUALS, "<=");
+			put(LexicalUnit.GREATER_SIGN, ">");
+			put(LexicalUnit.GREATER_OR_EQUALS, ">=");
+			put(LexicalUnit.MINUS_SIGN, "\\-");
+			put(LexicalUnit.PLUS_SIGN, "\\+");
+
+			put(LexicalUnit.IDENTIFIER, "[A-Za-z][0-9A-Za-z_\\-]{0,15}");
+			put(LexicalUnit.IMAGE, "s?9(?:\\([1-9][0-9]*\\))?(?:v9(?:\\([1-9][0-9]*\\))?)?");
+			put(LexicalUnit.INTEGER, "(?:\\+|-)?(?:[1-9][0-9]*)|(?:0)");
+			put(LexicalUnit.REAL, "(?:\\+|\\-)?(?:[1-9][0-9]*)|(?:0)(?:\\.[0-9]*[1-9])?");
+			put(LexicalUnit.STRING, "’[0-9A-Za-z\\+\\-\\*/:!\\? ]*’");
+
+			put(LexicalUnit.COMMENT, "(?:\\*|/).*\\.\\n");
+		}
+	};
 }
