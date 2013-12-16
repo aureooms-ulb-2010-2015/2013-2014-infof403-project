eca0665dc116607d8d3a518d61b58647f0fe199e
Ooms Aurélien <aurelien.ooms@gmail.com>
2013-12-17 00:43:41 +0100

    neg

---
+++ 2/more/src/cs/parser/binary/Neg.java
@@ -0,0 +1,18 @@
+package cs.parser.binary;
+
+import cs.parser.variable.*;
+
+public class Neg{
+	private IntegerVariable var_0;
+	private String var_1;
+	
+	public Neg(IntegerVariable var_0, String var_1){
+		this.var_0 = var_0;
+		this.var_1 = var_1;
+		this.genCode();
+	}
+
+	public void genCode(){
+		System.out.printf("%s = sub %s 0, %s\n", this.var_0.getName(), this.var_0.getType(), this.var_1);
+	}
+}
---
+++ 2/more/tools/c/neg.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+
+  int c, b;
+  b = -c;
+
+  return 0;
+}
+
